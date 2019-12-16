c71116c500870b2860d55012547c73252f61e1dd
erezrokah <erezrokah@users.noreply.github.com>
2019-12-16 10:51:52 +0200

    feat: add authorization for create-user

--- netlify-functions/create-user.js
+++ netlify-functions/create-user.js
@@ -1,18 +1,48 @@
 const fetch = require('node-fetch');
 
+const unauthorized = {
+  statusCode: 401,
+  body: JSON.stringify({ message: 'Unauthorized' }),
+};
+
 exports.handler = async (event, context) => {
-  console.log(JSON.stringify({ event, context }));
+  console.log(
+    'Create user input:',
+    JSON.stringify({ event, context }, null, 2),
+  );
+
+  const { authorization } = event.headers;
+  if (!authorization) {
+    return unauthorized;
+  }
+
   const { identity } = context.clientContext;
-  const usersUrl = `${identity.url}/admin/users`;
-  const adminAuthHeader = 'Bearer ' + identity.token;
+  const siteUrl = identity.url.split('/.netlify/identity')[0];
+
+  // authorization check based on Netlify API Token
+  const sites = await fetch('https://api.netlify.com/api/v1/sites/', {
+    headers: {
+      authorization,
+    },
+  });
+  console.log('Create user sites:', JSON.stringify(sites, null, 2));
+  const site = sites.find(site => site.ssl_url === siteUrl);
+  if (!site) {
+    return unauthorized;
+  }
 
   try {
+    // create user
+    const usersUrl = `${identity.url}/admin/users`;
+    const adminAuthHeader = 'Bearer ' + identity.token;
+    const { email, password } = JSON.parse(event.body);
+
     return fetch(usersUrl, {
       method: 'POST',
       headers: { Authorization: adminAuthHeader },
       body: JSON.stringify({
-        email: 'test-email@test.com',
-        password: 'test-email',
+        email,
+        password,
       }),
     })
       .then(response => {
@@ -20,10 +50,12 @@ exports.handler = async (event, context) => {
       })
       .then(data => {
         console.log('Created a user! 204!');
-        console.log(JSON.stringify({ data, event }));
-        return { statusCode: 204 };
+        return { statusCode: 204, body: JSON.stringify(data) };
       });
   } catch (e) {
-    return e;
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Unknown error' }),
+    };
   }
 };
